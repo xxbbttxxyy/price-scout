e26770061a6cd6764b62be592ec2b1e4356a5ec5
xxbbttxxyy <lilspanishkid09@gmail.com>
2026-01-14 22:28:16 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
- function App() {
+function App() {
   return (
-    <div style={{ padding: "2rem" }}>
+    <div style={{
+      padding: "3rem",
+      fontFamily: "system-ui",
+      maxWidth: "800px",
+      margin: "0 auto"
+    }}>
       <h1>Price Scout</h1>
-      <p>If you see this, React is working.</p>
+      <p>Track prices. Compare smarter.</p>
     </div>
   );
 }
-
-export default App;
